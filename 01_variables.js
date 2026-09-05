86572288744c3d144126665567aa7ff617aaabe8
X-saurav242 <sauravkumarsingh613@gmail.com>
2026-09-05 19:33:07 +0000

    conier config

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountemail = "hitesh@google.com"
+var accountpassword = "12345"
+accountcity = "jaipur"
+
+
+accountemail = "hc@hc.com"
+accountpassword = "21212121"
+accountcity = "bengaluru"
+/*
+  prefer noy to use var because of issue in block scope and
+*/
+console.log(accountId);
+console.log(accountemail);
+console.log(accountpassword);
+console.table([accountId, accountemail, accountpassword, accountcity]);
